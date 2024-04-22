559ad3ee2209f58ca7461e36d71110877d36655c
Tiberioursan <tiberioursan@gmail.com>
2024-04-22 11:32:11 +0200

    Added Storybook and created four CardComponent examples

---
+++ src/components/CardComponent.stories.js
@@ -0,0 +1,65 @@
+import CardComponent from './CardComponent.vue';
+
+export default {
+    title: 'Case/Teaser Card Component',
+    component: CardComponent,
+};
+
+const Template = (args) => ({
+    components: { CardComponent },
+    setup() {
+        return { args };
+    },
+    template: '<card-component v-bind="args" />',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+    id: 1,
+    title: 'Card Title longer than 30 characters',
+    ingress: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    preTitle: 'Pre Title:',
+    badge: 'Badge',
+    image: 'https://www.fjordtravel.no/wp-content/uploads/2013/08/Amazing-Geirangerfjord-by-Pixabay.jpg',
+    tags: ['Tag1', 'Tag2'],
+    cardType: 'horizontalCard',
+};
+
+export const HorizontalCard = Template.bind({});
+HorizontalCard.args = {
+    id: 1,
+    title: 'Card Title longer than 30 characters',
+    ingress: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    preTitle: 'Pre Title:',
+    image: 'https://www.fjordtravel.no/wp-content/uploads/2013/08/Amazing-Geirangerfjord-by-Pixabay.jpg',
+    tags: ['Tag1', 'Tag2'],
+    cardType: 'horizontalCard',
+};
+export const VerticalCard = Template.bind({});
+VerticalCard.args = {
+    id: 2,
+    title: 'Card Title longer than 30 characters',
+    ingress: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    preTitle: 'Pre Title:',
+    image: 'https://www.fjordtravel.no/wp-content/uploads/2013/08/Amazing-Geirangerfjord-by-Pixabay.jpg',
+    tags: ['Tag1', 'Tag2'],
+    cardType: 'verticalCard',
+};
+export const BasicCard = Template.bind({});
+BasicCard.args = {
+    id: 3,
+    title: 'Card Title longer than 30 characters',
+    ingress: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    tags: ['Tag1', 'Tag2'],
+    cardType: 'basicCard',
+};
+export const DetailedCard = Template.bind({});
+DetailedCard.args = {
+    id: 4,
+    title: 'Card Title longer than 30 characters',
+    ingress: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    badge: 'Badge',
+    image: 'https://www.fjordtravel.no/wp-content/uploads/2013/08/Amazing-Geirangerfjord-by-Pixabay.jpg',
+    tags: ['Tag1', 'Tag2'],
+    cardType: 'detailedCard',
+};
